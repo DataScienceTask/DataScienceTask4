7db420335aca618102998320e3347e795066b414
vinwun <221407327+vinwun@users.noreply.github.com>
2026-06-09 18:12:11 +0200

    mergesort.py cleanup

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,58 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
 import matplotlib.pyplot as plt
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+def merge_sort(arr):
+    """
+    Sorts arr recursively using merge sort.
+    """
+    if len(arr) < 2:
+        return
+
+    # split list into halves
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # sort halves
+    merge_sort(left)
+    merge_sort(right)
+
+
+    left_i = right_i = total_i = 0
+
+    # merge while both lists have elements
+    while left_i < len(left) and right_i < len(right):
+        if left[left_i] <= right[right_i]:
+            arr[total_i] = left[left_i]
+            left_i += 1
+        else:
+            arr[total_i] = right[right_i]
+            right_i += 1
+        total_i += 1
+
+    # merge remaining left elements
+    while left_i < len(left):
+        arr[total_i] = left[left_i]
+        left_i += 1
+        total_i += 1
+
+    # merge remaining right elements
+    while right_i < len(right):
+        arr[total_i] = right[right_i]
+        right_i += 1
+        total_i += 1
+
+
+def plot_linear(arr):
+    """
+    Plots arr on a linear scale.
+    """
+    plt.plot(arr)
+    plt.show()
+
+
+if __name__ == "__main__":
+    data = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    plot_linear(data)
+    merge_sort(data)
+    plot_linear(data)
